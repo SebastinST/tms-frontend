f02556b8b8688885c7f9bf1e2e64d1de2fa94642
SebastinST <mingchensebastin.goh@stengg.com>
2023-11-27 17:59:31 +0800

    U2 add get and update user details on profile page

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,7 +1,71 @@
+// Add cannot press save if no edits
+
 import './Profile.css';
 import Navbar from '../components/Navbar';
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+import Cookies from 'js-cookie';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Profile() {
+    const [profile, setProfile] = useState({});
+    const [inputs, setInputs] = useState({});
+    const [editing, setEditing] = useState(false);
+    
+    useEffect(() => {
+        async function getSelf() {
+            try {
+                let result = await Axios.get('http://localhost:8000/getSelf',{
+                    headers: { Authorization: `Bearer ${Cookies.get('jwt-token')}` }
+                });
+                setProfile(result.data.data);
+            } catch (e) {
+                let error = e.response.data
+                if (error) {
+                    
+                    // Show error message
+                    toast.error(error.message, {
+                        autoClose: false,
+                    });
+                }
+            }
+        }
+        getSelf();
+    }, [editing]);
+
+    const startEditing = () => {
+        setInputs(values => ({...values, 'email': profile.email}));
+        setEditing(true);
+    }
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values, [name]: value}));
+    }
+
+    const handleSubmit = async() => {
+        try {
+            let result = await Axios.post(
+                'http://localhost:8000/updateSelf',
+                inputs, 
+                {headers: { Authorization: `Bearer ${Cookies.get('jwt-token')}`}}
+            );
+            toast.success(result.data.message);
+            setEditing(false);
+        } catch (e) {
+            let error = e.response.data
+            if (error) {
+                // Show error message
+                toast.error(error.message, {
+                    autoClose: false,
+                });
+            }
+        }
+    }
+
     return (
         <>
         <Navbar />
@@ -9,18 +73,35 @@ function Profile() {
             <span className="title">My Account</span>
             <div className="profile-form">
                 <div>
-                    <div>Username: User1</div>
                     <div>
-                        Email: User1@gmail.com
+                        Username: {profile.username}
                     </div>
-                    <div>Password: ***********</div>
+                    <div>
+                        Email: {
+                        editing
+                        ? <input type='text' name='email' value={inputs.email} onChange={handleChange}/>
+                        : profile.email
+                        }
+                    </div>
+                    <div>Password: {
+                        editing
+                        ? <input type='password' name='password' value={inputs.password || ""} onChange={handleChange} placeholder='Input new password'/>
+                        : '****************'
+                        }
+                    </div>
                 </div>
                 <div>
                     <div>User Groups: dev, admin</div>
-                    <input className="profile-submit" type="submit" value="Edit"/>
+                    {editing
+                    ? <input className="profile-submit" type="button" onClick={handleSubmit} value="Save"
+                        {...profile.email === inputs.email && `disabled`}
+                    />
+                    : <button className="profile-submit" type="button" onClick={startEditing}>Edit</button> 
+                    }
                 </div>
             </div>
         </div>
+        <ToastContainer closeOnClick theme="colored"/>
         </>
     );
 }
