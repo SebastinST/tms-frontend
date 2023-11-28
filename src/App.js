741cedd075ebb444cf2843f980c7e7b632635c07
Jeremiah <therealjeremiah@gmail.com>
2023-11-28 22:22:59 +0800

    less laggy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import Login from "./Login.js";
-import Home from "./Home.js";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AdminHome from "./AdminHome.js";
-import MyAccount from "./MyAccount.js";
+import Login from "./Login.js"
+import Home from "./Home.js"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import AdminHome from "./AdminHome.js"
+import Admin2Home from "./Admin2Home.js"
+import MyAccount from "./MyAccount.js"
 
 function App() {
   return (
@@ -11,10 +12,11 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/adminhome" element={<AdminHome />} />
+        <Route path="/admin2home" element={<Admin2Home />} />
         <Route path="/myaccount" element={<MyAccount />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
