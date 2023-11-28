00c032dffdea3e07bad86a6dcf1cfe6cde1f9dee
SebastinST <mingchensebastin.goh@stengg.com>
2023-11-28 13:57:47 +0800

    O add catchall for rerouting when going to unserved routes depending on user login

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import User from './user/User';
 import Admin from './admin/Admin';
 import Profile from './profile/Profile';
 import CheckLogin from './components/CheckLogin';
+import CatchAll from './components/CatchAll';
 
 function App() {
 
@@ -17,7 +18,7 @@ function App() {
             <Route path="/user" element={<CheckLogin><User /></CheckLogin>} />
             <Route path="/admin" element={<CheckLogin><Admin /></CheckLogin>} />
             <Route path="/profile" element={<CheckLogin><Profile /></CheckLogin>} />
-            <Route path="*" element={<Login />} />
+            <Route path="*" element={<CatchAll />} />
           </Routes>
         </BrowserRouter>
       </div>
