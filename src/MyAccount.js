9208c60615342f4da6af84844a2c000785690e65
Roy <weichai.royston.yeo@stengg.com>
2023-11-30 11:05:41 +0800

    update myaccount ui

--- src/MyAccount.js
+++ src/MyAccount.js
@@ -1,113 +1,134 @@
-import * as React from "react"
-import Button from "@mui/material/Button"
-import CssBaseline from "@mui/material/CssBaseline"
-import Grid from "@mui/material/Grid"
-import Stack from "@mui/material/Stack"
-import Box from "@mui/material/Box"
-import Toolbar from "@mui/material/Toolbar"
-import Typography from "@mui/material/Typography"
-import Container from "@mui/material/Container"
-import Link from "@mui/material/Link"
-import { createTheme, ThemeProvider } from "@mui/material/styles"
-import Appbar from "./Appbar"
-import { useLocation } from "react-router-dom"
-import TextField from "@mui/material/TextField"
-import Cookies from "js-cookie"
-import { useNavigate } from "react-router-dom"
-import { useState, useEffect } from "react"
-import axios from "axios"
-import Alert from "@mui/material/Alert"
-import { ToastContainer, toast } from "react-toastify"
-import DispatchContext from "./DispatchContext"
+import * as React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Appbar from "./Appbar";
+import { useLocation } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Alert from "@mui/material/Alert";
+import { ToastContainer, toast } from "react-toastify";
+import DispatchContext from "./DispatchContext";
 
-const defaultTheme = createTheme()
+const defaultTheme = createTheme();
 
 export default function MyAccount() {
   const [defAccInfo, setDefAccInfo] = useState({
     username: "",
     email: "",
     group_list: "",
-    password: ""
-  })
-  const [fieldDisabled, setFieldDisabled] = useState(true)
-  const [editButton, setEditButton] = useState("Edit")
-  const [errorMessage, setErrorMessage] = useState("")
-  const [open, setOpen] = React.useState(false)
-  const { state } = useLocation()
-  const appDispatch = React.useContext(DispatchContext)
+    password: "",
+  });
+  const [fieldDisabled, setFieldDisabled] = useState(true);
+  const [editButton, setEditButton] = useState("Edit");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [open, setOpen] = React.useState(false);
+  const { state } = useLocation();
+  const appDispatch = React.useContext(DispatchContext);
   //Authorization
   const config = {
     headers: {
-      Authorization: "Bearer " + Cookies.get("token")
-    }
-  }
+      Authorization: "Bearer " + Cookies.get("token"),
+    },
+  };
   //get default account values
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get("http://localhost:8080/controller/getUser/", config)
+        const response = await axios.get("http://localhost:8080/controller/getUser/", config);
         //set password in response to empty
-        response.data.data.password = ""
-        setDefAccInfo(response.data.data)
+        response.data.data.password = "";
+        setDefAccInfo(response.data.data);
       } catch (err) {
-        appDispatch({ type: err.response.status })
+        appDispatch({ type: err.response.status });
       }
     }
-    fetchData()
-  }, [])
-  const handleSubmit = async event => {
-    event.preventDefault()
+    fetchData();
+  }, []);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
     if (fieldDisabled) {
-      setFieldDisabled(false)
-      setEditButton("Save")
+      setFieldDisabled(false);
+      setEditButton("Save");
     } else {
-      const data = new FormData(event.currentTarget)
-      const updateEmail = { email: data.get("email") }
+      const data = new FormData(event.currentTarget);
+      const updateEmail = { email: data.get("email") };
       try {
-        const res = await axios.put("http://localhost:8080/controller/updateUserEmail/", updateEmail, config)
+        const res = await axios.put(
+          "http://localhost:8080/controller/updateUserEmail/",
+          updateEmail,
+          config
+        );
         if (data.get("password") !== null && data.get("password") !== "") {
-          const updatePassword = { password: data.get("password") }
+          const updatePassword = { password: data.get("password") };
 
-          await axios.put("http://localhost:8080/controller/updateUserPassword/", updatePassword, config)
+          await axios.put(
+            "http://localhost:8080/controller/updateUserPassword/",
+            updatePassword,
+            config
+          );
         }
-        setFieldDisabled(true)
-        setEditButton("Edit")
+        setFieldDisabled(true);
+        setEditButton("Edit");
         //if password field is not empty, set it to empty
         setDefAccInfo({
           ...defAccInfo,
-          password: ""
-        })
-        setOpen(false)
-        setErrorMessage("")
-        toast.success("Account updated successfully")
+          password: "",
+        });
+        setOpen(false);
+        setErrorMessage("");
+        toast.success("Account updated successfully");
       } catch (error) {
-        setErrorMessage(error.response.data.errMessage)
-        setOpen(true)
+        setErrorMessage(error.response.data.errMessage);
+        setOpen(true);
       }
     }
-  }
+  };
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
       <Appbar title="My Account" group={state.group} />
       <main>
-        <ToastContainer position="top-center" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
 
         <Box
           sx={{
             marginTop: 8,
             display: "flex",
             flexDirection: "column",
-            alignItems: "center"
+            alignItems: "center",
           }}
         >
-          <Typography component="h1" variant="h4">
-            Username: {defAccInfo.username}
-          </Typography>
-          <Typography component="h1" variant="h4">
-            Groups: {defAccInfo.group_list}
-          </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <Box sx={{ border: 1, align: "left", p: 1 }}>
+              <Typography component="h1" variant="h5">
+                Username: {defAccInfo.username}
+              </Typography>
+              <Typography component="h1" variant="h5">
+                Groups: {defAccInfo.group_list}
+              </Typography>
+            </Box>
             <TextField
               margin="normal"
               required
@@ -117,10 +138,10 @@ export default function MyAccount() {
               name="email"
               autoComplete="email"
               value={defAccInfo.email}
-              onChange={e =>
+              onChange={(e) =>
                 setDefAccInfo({
                   ...defAccInfo,
-                  email: e.target.value
+                  email: e.target.value,
                 })
               }
               disabled={fieldDisabled}
@@ -136,10 +157,10 @@ export default function MyAccount() {
               id="password"
               value={defAccInfo.password}
               disabled={fieldDisabled}
-              onChange={e =>
+              onChange={(e) =>
                 setDefAccInfo({
                   ...defAccInfo,
-                  password: e.target.value
+                  password: e.target.value,
                 })
               }
             />
@@ -156,5 +177,5 @@ export default function MyAccount() {
         </Box>
       </main>
     </ThemeProvider>
-  )
+  );
 }
