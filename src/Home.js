c3d3755d8f906e974ba3e7ea5bf2d30cff025927
Roy <weichai.royston.yeo@stengg.com>
2023-11-30 10:23:09 +0800

    update user home

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,40 @@
-import * as React from "react"
-import Button from "@mui/material/Button"
-import CssBaseline from "@mui/material/CssBaseline"
-import Grid from "@mui/material/Grid"
-import Stack from "@mui/material/Stack"
-import Box from "@mui/material/Box"
-import Toolbar from "@mui/material/Toolbar"
-import Typography from "@mui/material/Typography"
-import Container from "@mui/material/Container"
-import Link from "@mui/material/Link"
-import { createTheme, ThemeProvider } from "@mui/material/styles"
-import Appbar from "./Appbar"
+import * as React from "react";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Appbar from "./Appbar";
 
-const defaultTheme = createTheme()
+const defaultTheme = createTheme();
 
 export default function Home() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
-      <Appbar title="Home"/>
+      <Appbar title="Home" />
       <main>
         {/* Hero unit */}
         <Box
           sx={{
             bgcolor: "background.paper",
             pt: 8,
-            pb: 6
+            pb: 6,
           }}
         >
           <Container maxWidth="sm"></Container>
         </Box>
-        <Container sx={{ py: 8 }} maxWidth="md"></Container>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          <Typography variant="h1" align="center">
+            [List of Apps]
+          </Typography>
+        </Container>
       </main>
     </ThemeProvider>
-  )
+  );
 }
